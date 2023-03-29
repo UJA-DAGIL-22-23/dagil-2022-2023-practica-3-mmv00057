31939cdf4b91df6baebc7fbec0f4d760c4ebd292
Modesto Monzón Vázquez <mmv00057@red.ujaen.es>
2023-03-29 18:09:58 +0200

    IMPLEMENTACION DEL CASO DE USO Nº4 V1.2

--- front-end/static-files/js/ms-plantilla.js
+++ front-end/static-files/js/ms-plantilla.js
@@ -1,3 +1,4 @@
+
 /**
  * @file Plantilla.js
  * @description Funciones para el procesamiento de la info enviada por el MS Plantilla
@@ -10,87 +11,19 @@
 /// Creo el espacio de nombres
 let Plantilla = {};
 
-// Plantilla de datosDescargados vacíos
-Plantilla.datosDescargadosNulos = {
-    mensaje: "Datos Descargados No válidos",
+// Plantilla de dDescargados vacíos
+Plantilla.dDescargadosNulos = {
+    mensaje: "d Descargados No válidos",
     autor: "",
     email: "",
     fecha: ""
 }
 
 
-/// Objeto para almacenar los datos de la persona que se está mostrando
-Plantilla.personaMostrada = null
-
-/// Plantilla para poner los datos de una persona en un tabla dentro de un formulario
-Plantilla.plantillaFormularioPersona = {}
-
-/**
- * Imprime los datos de una persona como una tabla dentro de un formulario usando la plantilla del formulario.
- * @param {persona} Persona Objeto con los datos de la persona
- * @returns Una cadena con la tabla que tiene ya los datos actualizados
- */
-Plantilla.personaComoFormulario = function (persona) {
-    return Plantilla.plantillaFormularioPersona.actualiza( persona );
-}
-
-/**
- * Almacena los datos de la persona que se está mostrando
- * @param {Persona} persona Datos de la persona a almacenar
- */
-
-Plantilla.almacenaDatos = function (persona) {
-    Plantilla.personaMostrada = persona;
-}
-
-/**
- * Función para mostrar en pantalla los detalles de una persona que se ha recuperado de la BBDD por su id
- * @param {Persona} persona Datos de la persona a mostrar
- */
-
-Plantilla.imprimeUnaPersona = function (persona) {
-    // console.log(persona) // Para comprobar lo que hay en vector
-    let msj = Plantilla.personaComoFormulario(persona);
-
-    // Borro toda la info de Article y la sustituyo por la que me interesa
-    Frontend.Article.actualizar("Mostrar una persona", msj)
-
-    // Actualiza el objeto que guarda los datos mostrados
-    Plantilla.almacenaDatos(persona)
-}
-
-/**
- * Función que recuperar todas las personas llamando al MS Personas. 
- * Posteriormente, llama a la función callBackFn para trabajar con los datos recuperados.
- * @param {String} idPersona Identificador de la persona a mostrar
- * @param {función} callBackFn Función a la que se llamará una vez recibidos los datos.
- */
-Plantilla.recuperaUnaPersona = async function (idPersona, callBackFn) {
-    try {
-        const url = Frontend.API_GATEWAY + "/plantilla/getPorId/" + idPersona
-        const response = await fetch(url);
-        if (response) {
-            const persona = await response.json()
-            callBackFn(persona)
-        }
-    } catch (error) {
-        alert("Error: No se han podido acceder al API Gateway")
-        console.error(error)
-    }
-}
-
-/**
- * Función principal para mostrar los datos de una persona desde el MS y, posteriormente, imprimirla.
- * @param {String} idPersona Identificador de la persona a mostrar
- */
-Plantilla.mostrar = function (idPersona) {
-    this.recuperaUnaPersona(idPersona, this.imprimeUnaPersona);
-}
-
 /**
  * Función que descarga la info MS Plantilla al llamar a una de sus rutas
  * @param {string} ruta Ruta a descargar
- * @param {función} callBackFn Función a la que se llamará una vez recibidos los datos.
+ * @param {función} callBackFn Función a la que se llamará una vez recibidos los d.
  */
 Plantilla.descargarRuta = async function (ruta, callBackFn) {
     let response = null
@@ -107,53 +40,53 @@ Plantilla.descargarRuta = async function (ruta, callBackFn) {
     }
 
     // Muestro la info que se han descargado
-    let datosDescargados = null
+    let dDescargados = null
     if (response) {
-        datosDescargados = await response.json()
-        callBackFn(datosDescargados)
+        dDescargados = await response.json()
+        callBackFn(dDescargados)
     }
 }
 
 
 /**
- * Función principal para mostrar los datos enviados por la ruta "home" de MS Plantilla
+ * Función principal para mostrar los d enviados por la ruta "home" de MS Plantilla
  */
-Plantilla.mostrarHome = function (datosDescargados) {
-    // Si no se ha proporcionado valor para datosDescargados
-    datosDescargados = datosDescargados || this.datosDescargadosNulos
+Plantilla.mostrarHome = function (dDescargados) {
+    // Si no se ha proporcionado valor para dDescargados
+    dDescargados = dDescargados || this.dDescargadosNulos
 
-    // Si datos descargados NO es un objeto 
-    if (typeof datosDescargados !== "object") datosDescargados = this.datosDescargadosNulos
+    // Si d descargados NO es un objeto 
+    if (typeof dDescargados !== "object") dDescargados = this.dDescargadosNulos
 
-    // Si datos descargados NO contiene el campo mensaje
-    if (typeof datosDescargados.mensaje === "undefined") datosDescargados = this.datosDescargadosNulos
+    // Si d descargados NO contiene el campo mensaje
+    if (typeof dDescargados.mensaje === "undefined") dDescargados = this.dDescargadosNulos
 
-    Frontend.Article.actualizar("Plantilla Home", datosDescargados.mensaje)
+    Frontend.Article.actualizar("Plantilla Home", dDescargados.mensaje)
 }
 
 /**
- * Función principal para mostrar los datos enviados por la ruta "acerca de" de MS Plantilla
+ * Función principal para mostrar los d enviados por la ruta "acerca de" de MS Plantilla
  */
-Plantilla.mostrarAcercaDe = function (datosDescargados) {
-    // Si no se ha proporcionado valor para datosDescargados
-    datosDescargados = datosDescargados || this.datosDescargadosNulos
+Plantilla.mostrarAcercaDe = function (dDescargados) {
+    // Si no se ha proporcionado valor para dDescargados
+    dDescargados = dDescargados || this.dDescargadosNulos
 
-    // Si datos descargados NO es un objeto 
-    if (typeof datosDescargados !== "object") datosDescargados = this.datosDescargadosNulos
+    // Si d descargados NO es un objeto 
+    if (typeof dDescargados !== "object") dDescargados = this.dDescargadosNulos
 
-    // Si datos descargados NO contiene los campos mensaje, autor, o email
-    if (typeof datosDescargados.mensaje === "undefined" ||
-        typeof datosDescargados.autor === "undefined" ||
-        typeof datosDescargados.email === "undefined" ||
-        typeof datosDescargados.fecha === "undefined"
-    ) datosDescargados = this.datosDescargadosNulos
+    // Si d descargados NO contiene los campos mensaje, autor, o email
+    if (typeof dDescargados.mensaje === "undefined" ||
+        typeof dDescargados.autor === "undefined" ||
+        typeof dDescargados.email === "undefined" ||
+        typeof dDescargados.fecha === "undefined"
+    ) dDescargados = this.dDescargadosNulos
 
     const mensajeAMostrar = `<div>
-    <p>${datosDescargados.mensaje}</p>
+    <p>${dDescargados.mensaje}</p>
     <ul>
-        <li><b>Autor/a</b>: ${datosDescargados.autor}</li>
-        <li><b>E-mail</b>: ${datosDescargados.email}</li>
-        <li><b>Fecha</b>: ${datosDescargados.fecha}</li>
+        <li><b>Autor/a</b>: ${dDescargados.autor}</li>
+        <li><b>E-mail</b>: ${dDescargados.email}</li>
+        <li><b>Fecha</b>: ${dDescargados.fecha}</li>
     </ul>
     </div>
     `;
@@ -175,84 +108,14 @@ Plantilla.procesarAcercaDe = function () {
     this.descargarRuta("/plantilla/acercade", this.mostrarAcercaDe);
 }
 
-// Tags que voy a usar para sustituir los campos
-Plantilla.plantillaTags = {
-    "ID": "### ID ###",
-    "NOMBRE": "### NOMBRE ###",
-    "APELLIDOS": "### APELLIDOS ###",
-    "DIRECCION": "### DIRECCION ###",
-    "AÑOS PARTICIPACION": "### AÑOS PARTICIPACION ###",
-    "PARTICIPACIONES OLIMPICAS" : "### PARTICIPACIONES OLIMPICAS ###",
-    "TIPO": "### TIPO ###",
-}
-
 /**
- * Actualiza el cuerpo de la plantilla deseada con los datos de la persona que se le pasa
- * @param {String} Plantilla Cadena conteniendo HTML en la que se desea cambiar lso campos de la plantilla por datos
- * @param {Persona} Persona Objeto con los datos de la persona que queremos escribir en el TR
- * @returns La plantilla del cuerpo de la tabla con los datos actualizados 
- */  
-Plantilla.sustituyeTags = function (plantilla, persona) {
-    return plantilla
-        .replace(new RegExp(Plantilla.plantillaTags.ID, 'g'), persona.ref['@ref'].id)
-        .replace(new RegExp(Plantilla.plantillaTags.NOMBRE, 'g'), persona.data.nombreCompleto.nombre)
-        .replace(new RegExp(Plantilla.plantillaTags.APELLIDOS, 'g'), persona.data.nombreCompleto.apellido)
-        .replace(new RegExp(Plantilla.plantillaTags.DIRECCION, 'g'), persona.data.direccion.calle + ","+ persona.data.direccion.localidad + "," +
-         persona.data.direccion.provincia + "," + persona.data.direccion.pais)
-        .replace(new RegExp(Plantilla.plantillaTags["AÑOS PARTICIPACION"], 'g'), persona.data.aniosParticipacionMundial)
-        .replace(new RegExp(Plantilla.plantillaTags["PARTICIPACIONES OLIMPICAS"], 'g'), persona.data.numeroParticipacionesOlimpicas)
-        .replace(new RegExp(Plantilla.plantillaTags.TIPO, 'g'), persona.data.tipo)
-
-    }
-
-
-/**
- * Actualiza el cuerpo de la tabla con los datos de la persona que se le pasa
- * @param {Persona} Persona Objeto con los datos de la persona que queremos escribir en el TR
- * @returns La plantilla del cuerpo de la tabla con los datos actualizados 
+ * Función que recuperar todos los proyectos llamando al MS Proyectos
+ * @param {función} callBackFn Función a la que se llamará una vez recibidos los d.
  */
-Plantilla.plantillaTablaPersonas.actualiza = function (persona) {
-    return Plantilla.sustituyeTags(this.cuerpo, persona)
-}
-
-/**
- * Actualiza el formulario con los datos de la persona que se le pasa
- * @param {Persona} Persona Objeto con los datos de la persona que queremos escribir en el TR
- * @returns La plantilla del cuerpo de la tabla con los datos actualizados 
- */
-Plantilla.plantillaFormularioPersona.actualiza = function (persona) {
-    return Personas.sustituyeTags(this.formulario, persona)
-}
-
-
-/**
- * Función para mostrar en pantalla todas las personas que se han recuperado de la BBDD.
- * @param {Vector_de_personas} vector Vector con los datos de las personas a mostrar
- */
-
-Plantilla.imprimeMuchasPersonas = function (vector) {
-    // console.log(vector) // Para comprobar lo que hay en vector
-
-    // Compongo el contenido que se va a mostrar dentro de la tabla
-    let msj = Plantilla.plantillaTablaPersonas.cabecera
-    vector.forEach(e => msj += Plantilla.plantillaTablaPersonas.actualiza(e) ,
-        console.log(e)
-    )
-    msj += Plantilla.plantillaTablaPersonas.pie
-
-    // Borro toda la info de Article y la sustituyo por la que me interesa
-    Frontend.Article.actualizar("Listado de personas", msj)
-}
-
-/**
- * Función que recuperar todas las personas llamando al MS Personas
- * @param {función} callBackFn Función a la que se llamará una vez recibidos los datos.
- */
-
 Plantilla.recupera = async function (callBackFn) {
     let response = null
 
-    // Intento conectar con el microservicio personas
+    // Intento conectar con el microservicio proyectos
     try {
         const url = Frontend.API_GATEWAY + "/plantilla/getTodas"
         response = await fetch(url)
@@ -263,78 +126,172 @@ Plantilla.recupera = async function (callBackFn) {
         //throw error
     }
 
-    // Muestro todas las persoans que se han descargado
-    let vectorPersonas = null
+    // Muestro todos los proyectos que se han descargado
+    let vectorProyectos = null
     if (response) {
-        vectorPersonas = await response.json()
-        callBackFn(vectorPersonas.data)
+        vectorProyectos = await response.json()
+        callBackFn(vectorProyectos.data)
     }
 }
 
+/*
+ * Función que recuperar todos los proyectos junto con las personas asignadas a cada uno de ellos
+ * llamando al MS Proyectos
+ * @param {función} callBackFn Función a la que se llamará una vez recibidos los d.
+ 
+Plantilla.recuperaConPersonas = async function (callBackFn) {
+    let response = null
+
+    // Intento conectar con el microservicio proyectos
+    try {
+        const url = Frontend.API_GATEWAY + "/plantilla/getTodosConPersonas"
+        response = await fetch(url)
+
+    } catch (error) {
+        alert("Error: No se han podido acceder al API Gateway")
+        console.error(error)
+        //throw error
+    }
+
+    // Muestro todos los proyectos que se han descargado
+    let vectorProyectos = null
+    if (response) {
+        vectorProyectos = await response.json()
+        callBackFn(vectorProyectos.data)
+    }
+}
+ */
+// Funciones para mostrar como TABLE
+
 /**
- * Función principal para recuperar las personas desde el MS y, posteriormente, imprimirlas.
+ * Crea la cabecera para mostrar la info como tabla
+ * @returns Cabecera de la tabla
+ */
+Plantilla.cabeceraTable = function () {
+    return `<table class="listado-personas">
+        <thead>
+        <th>Alias</th><th>Nombre</th><th>Apellidos</th><th>Dirección</th><th>Años participación</th><th>Número participaciones</th><th>Tipo</th> 
+        </thead>
+        <tbody>
+    `;
+}
+
+/**
+ * Muestra la información de cada proyecto en un elemento TR con sus correspondientes TD
+ * @param {persona} p d del proyecto a mostrar
+ * @returns Cadena conteniendo todo el elemento TR que muestra el proyecto.
+ */
+Plantilla.cuerpoTr = function (p) {
+    const d = p.data;
+    const nombre = d.nombre;
+    const apellidos = d.apellidos;
+    const direccion = d.direccion;
+    const añosParticipacion = d.añosParticipacion;
+    const numCompeticiones = d.numCompeticiones;
+    const tipo = d.tipo;
+
+    return `<tr title="${p.ref['@ref'].id}">
+    <td>${d.alias}</td>
+    <td>${nombre}</td>
+    <td> ${apellidos}</td>
+    <td>${direccion}</td>
+    <td>${añosParticipacion}</td>
+    <td>${numCompeticiones}</td>
+    <td>(${tipo})</td>
+    </tr>`;
+}
+
+
+/*
+ * Muestra la información de cada proyecto (incluyendo las personas asignadas) 
+ * en varios elementos TR con sus correspondientes TD
+ * @param {proyecto} p d del proyecto a mostrar
+ * @returns Cadena conteniendo los distintos elementos TR que muestran el proyecto.
+ 
+Plantilla.cuerpoConPersonasTr = function (p) {
+    const d = p.data
+    const ini = d.inicio;
+    const fin = d.final;
+    const presupuesto = Frontend.euros(d.presupuesto);
+    let msj = Plantilla.cabeceraTable();
+    msj += `<tr>
+    <td>${d.alias}</td>
+    <td><em>${d.nombre}</em></td>
+    <td>${presupuesto}</td>
+    <td>${ini.dia}/${ini.mes}/${ini.año}</td>
+    <td>${fin.dia}/${fin.mes}/${fin.año}</td>
+    </tr>
+    <tr><th colspan="5">Personas</th></tr>
+    <tr><td colspan="5">
+        ${d.d_personas
+            .map(e => "<a href='javascript:Personas.mostrar(\"" + e.ref['@ref'].id + "\")'>"
+                + e.data.nombre
+                + " " + e.data.apellidos
+                + "</a>")
+            .join(", ")}
+    </td></tr>
+    `;
+    msj += Plantilla.pieTable();
+    return msj;
+}
+*/
+
+/**
+ * Pie de la tabla en la que se muestran las personas
+ * @returns Cadena con el pie de la tabla
+ */
+Plantilla.pieTable = function () {
+    return "</tbody></table>";
+}
+
+
+/**
+ * Función para mostrar en pantalla todos los proyectos que se han recuperado de la BBDD.
+ * @param {Vector_de_proyectos} vector Vector con los d de los proyectos a mostrar
+ */
+Plantilla.imprime = function (vector) {
+    //console.log( vector ) // Para comprobar lo que hay en vector
+    let msj = "";
+    msj += Plantilla.cabeceraTable();
+    vector.forEach(e => msj += Plantilla.cuerpoTr(e))
+    msj += Plantilla.pieTable();
+
+    // Borro toda la info de Article y la sustituyo por la que me interesa
+    Frontend.Article.actualizar( "Listado de personas", msj )
+
+}
+
+
+
+/*
+ * Función para mostrar en pantalla todos los proyectos que se han recuperado de la BBD, 
+ * junto con las personas asignadas a los mismos.
+ * @param {Vector_de_proyectos} vector Vector con los d de los proyectos a mostrar
+ 
+Proyectos.imprimeConPersonas = function (vector) {
+    //console.log( vector ) // Para comprobar lo que hay en vector
+    let msj = "";
+    vector.forEach(e => msj += Plantilla.cuerpoConPersonasTr(e))
+
+    // Borro toda la info de Article y la sustituyo por la que me interesa
+    Frontend.Article.actualizar( "Listado de EQUIPOC con personas", msj )
+
+}
+*/
+/**
+ * Función principal para recuperar los proyectos desde el MS y, posteriormente, imprimirlos.
+ * @returns True
  */
 Plantilla.listar = function () {
-    Plantilla.recupera(Plantilla.imprimeMuchasPersonas);
+    this.recupera(this.imprime);
 }
 
-
-
-/**
- * Función que permite modificar los datos de una persona
- */
-Plantilla.editar = function () {
-    this.ocultarOpcionesSecundarias()
-    this.mostrarOcionesTerciariasEditar()
-    this.habilitarCamposEditables()
+/*
+ * Función principal para recuperar los proyectos, incluyendo las personas, desde el MS y, 
+ * posteriormente, imprimirlos.
+ * @returns True
+ 
+Plantilla.listarConPersonas = function () {
+    this.recuperaConPersonas(this.imprimeConPersonas);
 }
-
-/**
- * Función que permite cancelar la acción sobre los datos de una persona
- */
-Plantilla.cancelar = function () {
-    this.imprimeUnaPersona(this.recuperaDatosAlmacenados())
-    this.deshabilitarCamposEditables()
-    this.ocultarOcionesTerciariasEditar()
-    this.mostrarOpcionesSecundarias()
-}
-
-
-/**
- * Función para guardar los nuevos datos de una persona
- */
-Plantilla.guardar = async function () {
-    try {
-        let url = Frontend.API_GATEWAY + "/plantilla/setTodo/"
-        let id_persona = document.getElementById("form-persona-id").value
-        const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'no-cors', // no-cors, cors, *same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'omit', // include, *same-origin, omit
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrer: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify({
-                "id_persona": id_persona,
-                "nombre_persona": document.getElementById("form-persona-nombre").value,
-                "apellidos_persona": document.getElementById("form-persona-apellidos").value,
-                "email_persona": document.getElementById("form-persona-email").value,
-                "año_entrada_persona": document.getElementById("form-persona-anio").value
-            }), // body data type must match "Content-Type" header
-        })
-        /*
-        Error: No procesa bien la respuesta devuelta
-        if (response) {
-            const persona = await response.json()
-            alert(persona)
-        }
-        */
-        Plantilla.mostrar(id_persona)
-    } catch (error) {
-        alert("Error: No se han podido acceder al API Gateway " + error)
-        //console.error(error)
-    }
-}
+*/
--- ms-plantilla/callbacks.js
+++ ms-plantilla/callbacks.js
@@ -61,6 +61,87 @@ const CB_MODEL_SELECTS = {
             res.status(500).json({ error: error.description })
         }
     },
+/**
+     * Método para obtener todas las personas de la BBDD.
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    getTodas: async (req, res) => {
+        try {
+            let personas = await client.query(
+                q.Map(
+                    q.Paginate(q.Documents(q.Collection(COLLECTION))),
+                    q.Lambda("X", q.Get(q.Var("X")))
+                )
+            )   
+            // console.log( personas ) // Para comprobar qué se ha devuelto en personas
+            CORS(res)
+                .status(200)
+                .json(personas)
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
+ /**
+    * Método para obtener una persona de la BBDD a partir de su ID
+    * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+    * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+    */
+    getPorId: async (req, res) => {
+    try {
+            // console.log( "getPorId req", req.params.idPersona ) // req.params contiene todos los parámetros de la llamada
+            let persona = await client.query(
+                q.Get(q.Ref(q.Collection('Personas'), req.params.idPersona))
+           )
+            // console.log( persona ) // Para comprobar qué se ha devuelto en persona
+            CORS(res)
+                .status(200)
+                .json(persona)
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
+
+    /**
+    * Método para ocambiar los datos de una persona
+    * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+    * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+    */
+    setTodo: async (req, res) => {
+        //console.log("setTodo req.body", req) // req.body contiene todos los parámetros de la llamada
+        try {
+            let valorDevuelto = {}
+            // Hay que comprobar Object.keys(req.body).length para saber si req.body es objeto "normal" o con problemas
+            // Cuando la llamada viene de un formulario, se crea una sola entrada, con toda la info en una sola key y el value está vacío.
+            // Cuando la llamada se hace con un objeto (como se hace desde el server-spec.js), el value No está vacío.
+            let data = (Object.values(req.body)[0] === '') ? JSON.parse(Object.keys(req.body)[0]) : req.body
+            //console.log("SETTODO data es", data)
+            let persona = await client.query(
+                q.Update(
+                    q.Ref(q.Collection(COLLECTION), data.id_persona),
+                    {
+                        data: {
+                            nombre: data.nombre_persona,
+                            apellidos: data.apellidos_persona,
+                            email: data.email_persona,
+                            año_entrada: data.año_entrada_persona,
+                        },
+                    },
+                )
+            )
+                .then((ret) => {
+                    valorDevuelto = ret
+                    //console.log("Valor devuelto ", valorDevuelto)
+                    CORS(res)
+                        .status(200)
+                        .header( 'Content-Type', 'application/json' )
+                        .json(valorDevuelto)
+                })
+
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
 
 }
 
